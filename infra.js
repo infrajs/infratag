42f965849151f6730059a366776e45c75d61c227
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-05-10 10:10:40 +0400

    fix

--- infra.js
+++ infra.js
@@ -1,26 +1,32 @@
-(async () => {
-	if (!window.infra) window.infra = {};
-	infra.infratag = {};
-	infra.infratag.counter = 0;
-	infra.infratag.childs = {};
-	let CDN = (await import('/vendor/akiyatkin/load/CDN.js')).default
-	await CDN.load('jquery');
-	Event.handler('Layer.onshow', async (layer) => {
-		$(document.getElementById(layer.div)).find('[data-infra]').removeAttr('data-infra').each( function () {
-			if (!this.id) {
-				infra.infratag.counter++;
-				this.id = 'infratag' + infra.infratag.counter;
-			}
-			if (!infra.infratag.childs[this.id]) {
-				var child = $(this).data();
-				child.div = this.id;
-				//child.divparent=layer.div;
-				infra.infratag.childs[this.id]=child;
-			} else {
-				var child = infra.infratag.childs[this.id];
-			}
-			child.showed = false;
-			Controller.check(child);
-		});
+import { Event } from '/vendor/infrajs/event/Event.js'
+
+let Infratag = {}
+Infratag.counter = 0
+Infratag.childs = {}
+let ws = new WeakSet()
+Event.handler('Layer.onshow', async (layer) => {
+	if (!layer.div) return
+	let div = document.getElementById(layer.div)
+	let blocks = div.querySelectorAll('[data-infra]')
+	blocks.forEach(block => {
+		if (ws.has(block)) return
+		ws.add(block)
+
+		if (!block.id) {
+			Infratag.counter++;
+			block.id = 'infratag' + Infratag.counter;
+		}
+		if (!Infratag.childs[block.id]) {
+			let child = {};
+			for (name in block.dataset) child[name] = block.dataset[name]
+			child.div = block.id;
+			Infratag.childs[block.id] = child;
+		} else {
+			let child = Infratag.childs[block.id];
+		}
+		child.showed = false;
+		Controller.check(child);
 	});
-})();
+});
+
+export {Infratag}
